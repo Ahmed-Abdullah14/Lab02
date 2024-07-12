8539362225f232c2f6509cb88bdce99ff60699a2
Ahmed Abdullah <ahmed.abdullah1@ucalgary.ca>
2024-07-11 19:30:06 -0600

    Lab2 Section A

---
+++ src/Application.java
@@ -0,0 +1,25 @@
+/**
+ * <h1> Application.java</h1>
+ * <p>
+ * This class is designed for printing "Hello, World!" in Java
+ * It is part of Lab 2 Exercise A
+ * </p>
+ * 
+ * <p><b>Submission Date: July 11, 2024</p>
+ * 
+ * @author Ahmed Abdullah
+ * @version 1.0
+ */
+
+
+public class Application {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+
+	}
+
+}
